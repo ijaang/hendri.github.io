5d658636f77f1aa498f0f02ff76455decdde0237
ijaang <abuabudidinding@gmail.com>
2018-06-24 01:18:04 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// animasi smooth scroll
+$ ('#view-work').on('click', function(){
+    const images = $('#images').position().top;
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
+
+
+(function() {	
+    $.fatNav();		
+    }());
